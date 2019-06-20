4ef4d51e428472b16c9c9b54c2e3f1fea81db487
rudyhuynh <rudyhuynh@niometrics.com>
2019-06-20 19:44:48 +0800

    shorter

--- main.js
+++ main.js
@@ -1,37 +1,19 @@
 #!/usr/bin/env node
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
-
+const { version } = require("./package.json");
 async function main() {
-  const { stdout, stderr } = await exec("git branch");
+  console.info(`Version: ${version}`);
+
+  const { stdout, stderr } = await exec(
+    `git checkout master && git branch | grep -v "master" | xargs git branch -D`
+  );
   if (stderr) {
     return console.error(stderr);
   }
-  const branches = stdout
-    .toString()
-    .replace(/\*/g, "")
-    .split(/\s/)
-    .filter(Boolean)
-    .filter(branch => branch !== "master");
 
-  if (branches.length < 1) {
-    console.log("No local branch (except master) to be deleted\n");
-    return;
-  }
-  await exec("git checkout master");
-  console.log(
-    `Deleting ${branches.length} local branch${
-      branches.length > 1 ? "es" : ""
-    } except 'master'...`
-  );
-  await Promise.all(
-    branches.map(async branch => {
-      await exec(`git branch -D ${branch}`);
-    })
-  );
-
-  console.log("\nDONE!");
-  console.log("\n> git branch\n");
-  console.log((await exec("git branch")).stdout);
+  console.info("\nDONE!");
+  console.info("\n> git branch\n");
+  console.info((await exec("git branch")).stdout);
 }
 main();
