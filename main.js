1c91227271a2c1a591090c024e44984c663559f7
rudyhuynh <rudyhuynh@niometrics.com>
2019-06-20 19:56:08 +0800

    add version

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
+const { version } = require("./package.json");
 
 async function main() {
+  console.info(`Version: ${version}`);
   const { stdout, stderr } = await exec("git branch");
   if (stderr) {
     return console.error(stderr);
@@ -15,11 +17,11 @@ async function main() {
     .filter(branch => branch !== "master");
 
   if (branches.length < 1) {
-    console.log("No local branch (except master) to be deleted\n");
+    console.info("No local branch (except master) to be deleted\n");
     return;
   }
   await exec("git checkout master");
-  console.log(
+  console.info(
     `Deleting ${branches.length} local branch${
       branches.length > 1 ? "es" : ""
     } except 'master'...`
@@ -30,8 +32,8 @@ async function main() {
     })
   );
 
-  console.log("\nDONE!");
-  console.log("\n> git branch\n");
-  console.log((await exec("git branch")).stdout);
+  console.info("\nDONE!");
+  console.info("\n> git branch\n");
+  console.info((await exec("git branch")).stdout);
 }
 main();
