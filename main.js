44f659ead8dde9380a9f32fa7d89866e23429683
Rudy Huynh <rudyhuynh@niometrics.com>
2020-07-04 22:51:57 +0800

    remove console.log

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ async function main() {
   }
 
   const currentBranch = await getCurrentBranch();
-  console.log("currentBranch", currentBranch);
 
   const branches = stdout
     .toString()
@@ -24,8 +23,6 @@ async function main() {
     .split(/\s/)
     .filter(Boolean)
     .filter((branch) => branch !== "master" && branch !== currentBranch);
-
-  console.log("branches", branches.join(","));
 
   if (branches.length < 1) {
     console.info("No local branch (except master) to be deleted\n");
